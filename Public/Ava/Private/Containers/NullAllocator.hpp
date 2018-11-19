cfe3901875533aa51f3f292dabab671a051f064a
vasama <git@vasama.org>
2018-11-19 19:46:08 +0200

    Add NullAllocator comparison operators

--- Public/Ava/Private/Containers/NullAllocator.hpp
+++ Public/Ava/Private/Containers/NullAllocator.hpp
@@ -1,9 +1,23 @@
 #pragma once
 
+#include "Ava/Misc.hpp"
+
 namespace Ava::Private::Containers {
 
 struct NullAllocator
 {
 };
+
+constexpr Ava_FORCEINLINE bool operator==(
+	const NullAllocator&, const NullAllocator&)
+{
+	return true;
+}
+
+constexpr Ava_FORCEINLINE bool operator!=(
+	const NullAllocator&, const NullAllocator&)
+{
+	return false;
+}
 
 } // namespace Ava::Private::Containers
