a50780c22b7cd9197e19c0828255f3e60196d5d6
vasama <git@vasama.org>
2018-11-20 11:48:20 +0200

    Add CString Compare

--- Public/Ava/Utility/CString.hpp
+++ Public/Ava/Utility/CString.hpp
@@ -14,7 +14,10 @@ struct BasicCString
 	typedef TChar CharType;
 
 	static iword Size(const CharType* cstr);
-	static iword Size(const CharType* cstr, iword maxLength);
+	static iword Size(const CharType* cstr, iword maxSize);
+
+	static i32 Compare(const CharType* cstr1, const CharType* cstr2);
+	static i32 Compare(const CharType* cstr1, const CharType* cstr2, iword maxSize);
 
 	BasicCString() = delete;
 };
@@ -41,6 +44,30 @@ template<>
 Ava_FORCEINLINE iword BasicCString<wchar_t>::Size(const wchar_t* cstr, iword maxLength)
 {
 	return ::wcsnlen(cstr, maxLength);
+}
+
+template<>
+Ava_FORCEINLINE i32 BasicCString<char>::Compare(const char* cstr1, const char* cstr2)
+{
+	return ::strcmp(cstr1, cstr2);
+}
+
+template<>
+Ava_FORCEINLINE i32 BasicCString<char>::Compare(const char* cstr1, const char* cstr2, iword maxSize)
+{
+	return ::strncmp(cstr1, cstr2, maxSize);
+}
+
+template<>
+Ava_FORCEINLINE i32 BasicCString<wchar_t>::Compare(const wchar_t* cstr1, const wchar_t* cstr2)
+{
+	return ::wcscmp(cstr1, cstr2);
+}
+
+template<>
+Ava_FORCEINLINE i32 BasicCString<wchar_t>::Compare(const wchar_t* cstr1, const wchar_t* cstr2, iword maxSize)
+{
+	return ::wcsncmp(cstr1, cstr2, maxSize);
 }
 
 typedef BasicCString<char> CString;
